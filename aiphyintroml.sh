8fe8fd8d658be9e2959369a21a5ec6a7b40e5c92
Riccardo Finotello <riccardo.finotello@cea.fr>
2024-09-26 18:10:57 +0200

    Force overwriting

--- aiphyintroml.sh
+++ aiphyintroml.sh
@@ -1,7 +1,7 @@
 #! /bin/env bash
 
 # Download the docker file
-curl -o /tmp/aiphyintroml.dockerfile https://raw.githubusercontent.com/thesfinox/aiphy-intro-ml-homework/refs/heads/main/aiphyintroml.dockerfile
+curl -L https://raw.githubusercontent.com/thesfinox/aiphy-intro-ml-homework/refs/heads/main/aiphyintroml.dockerfile >| /tmp/aiphyintroml.dockerfile
 
 # Build and run the Docker image
 docker build --pull --rm -f "/tmp/aiphyintroml.dockerfile" -t aiphyintroml:latest "."
