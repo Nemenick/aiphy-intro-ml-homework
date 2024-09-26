45880189bf107800f73f6cf8958d164397c73339
Riccardo Finotello <riccardo.finotello@cea.fr>
2024-09-26 17:59:21 +0200

    Download the dockerfile

--- aiphyintroml.sh
+++ aiphyintroml.sh
@@ -1,5 +1,8 @@
 #! /bin/env bash
 
+# Download the docker file
+curl -o /tmp/aiphyintroml.dockerfile https://raw.githubusercontent.com/thesfinox/aiphy-intro-ml-homework/refs/heads/main/aiphyintroml.dockerfile
+
 # Build and run the Docker image
-docker build --pull --rm -f "aiphyintroml.dockerfile" -t aiphyintroml:latest "."
+docker build --pull --rm -f "/tmp/aiphyintroml.dockerfile" -t aiphyintroml:latest "."
 docker run -p 8888:8888 --rm -it  aiphyintroml:latest
